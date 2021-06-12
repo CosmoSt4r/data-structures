f105fc96b7c4f36664c8a9bae66f9111fa0c7909
Andrey Kalkutin <CosmoStar@protonmail.com>
2021-06-12 18:12:12 +0500

    Files deleted

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,11 @@
 ﻿#include <iostream>
+
 #include "DataStructures/DynamicArray.h"
 #include "DataStructures/DLinkedList.h"
 #include "DataStructures/Stack.h"
 #include "DataStructures/Queue.h"
+
+#include "Math/Fraction.h"
 
 int main() {
 
@@ -10,6 +13,7 @@ int main() {
 	DLinkedList<float> dllist;
 	Stack<double> stack;
 	Queue<int> queue;
+	Fraction fraction;
 
 	return 0;
 }
